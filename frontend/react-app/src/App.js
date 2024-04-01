38ce132c08211eeecda69b7cc934d1506c96975d
Thomas Ayrivié <99038943+ThomasAyr@users.noreply.github.com>
2024-04-01 21:23:38 +0000

    Udapte bicycle animation

--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -1,22 +1,41 @@
-//import logo from './favicon.ico';<img src={logo} className="App-logo" alt="logo" />
-import './App.css';
+import React from 'react';
+import './App.css'; 
+import bike from "./Graphic/bike.png"; 
+import bike2 from "./Graphic/bike2.png"; 
+import cityBackground from "./Graphic/city.jpg";
+
+const cardsData = [
+  { number: '01', text: 'First Feature' },
+  { number: '02', text: 'Second Sight' },
+  { number: '03', text: 'Third Wonder' },
+];
+
+function Card({ number, text }) {
+  return (
+    <div className="card">
+      <div className="card-number">{number}</div>
+      <div className="card-text">{text}</div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Montpellier Mobility Assistant</h1>
+        <div className="background-container">
+          <img src={cityBackground} className="city-background" alt="City" />
+          
+          <img src={bike} className="bike-logo" alt="Bike" />
+          <img src={bike2} className="bike2-logo" alt="Bike2" />
+        </div>
       </header>
+      <div className="cards-container">
+        {cardsData.map((card, index) => (
+          <Card key={index} number={card.number} text={card.text} />
+        ))}
+      </div>
     </div>
   );
 }
